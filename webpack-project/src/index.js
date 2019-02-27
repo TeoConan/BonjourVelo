2a6011b7f49205c645d9f90e3b8c29b648c96b3f
Teo Conan <teo.conan.tirault@gmail.com>
2019-02-27 15:38:52 +0100

    Components JS / CSS & all projects in one

--- webpack-project/src/index.js
+++ webpack-project/src/index.js
@@ -1,4 +1,62 @@
 require("../assets/stylesheets/scss/app.scss");
-console.log('Hello world JS 1');
+
+var selectButton = '.mdc-button';
+var selectTextField = '.mdc-text-field';
+var selectHelperText = '.mdc-text-field-helper-text';
+var selectTextFieldIcon = '.mdc-text-field-icon';
+var selectNotchedOutline = '.mdc-notched-outline';
+var selectFloatingLabel = '.mdc-floating-label';
+var selectSlider = '.mdc-slider';
+
+/* Buttons */
 import {MDCRipple} from '@material/ripple';
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+if ($(selectButton).length) {
+	const buttonRipple = new MDCRipple(document.querySelector(selectButton));
+}
+
+/* Script inputbox */
+import {MDCTextField} from '@material/textfield';
+
+if ($(selectTextField).length) {
+	var textFields = [];
+	var mdcTextFields = [];
+	$(selectTextField).each(function( index ) {
+	  	textFields[index] = this;
+	  	mdcTextFields[index] = new MDCTextField(this);
+	});
+}
+
+/* Helper Text */
+import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
+if ($(selectHelperText).length) {
+	const helperText = new MDCTextFieldHelperText(document.querySelector(selectHelperText));
+}
+
+/* Icon */
+import {MDCTextFieldIcon} from '@material/textfield/icon';
+if ($(selectTextFieldIcon).length) {
+	const icon = new MDCTextFieldIcon(document.querySelector(selectTextFieldIcon));
+}
+
+import {MDCNotchedOutline} from '@material/notched-outline';
+if ($(selectNotchedOutline).length) {
+	const notchedOutline = new MDCNotchedOutline(document.querySelector(selectNotchedOutline));
+}
+
+import {MDCFloatingLabel} from '@material/floating-label';
+if ($(selectFloatingLabel).length) {
+	const floatingLabel = new MDCFloatingLabel(document.querySelector(selectFloatingLabel));
+}
+
+import {MDCSlider} from '@material/slider';
+if ($(selectSlider).length) {
+
+	var sliders = [];
+	var mdcSliders = [];
+	$(selectSlider).each(function( index ) {
+	  	sliders[index] = this;
+	  	mdcSliders[index] = new MDCSlider(this);
+	  			//mdcSliders[index].listen('MDCSlider:change', () => console.log(`Value changed to ${mdcSliders[index].value}`));
+	});
+}
+
